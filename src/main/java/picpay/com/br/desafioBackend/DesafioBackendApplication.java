9d6f7768299fb1043d8c9e2c048c254c8359fa76
kp <kp@kps-MacBook-Air.local>
2024-05-12 22:05:39 -0300

    first commit

---
+++ src/main/java/picpay/com/br/desafioBackend/DesafioBackendApplication.java
@@ -0,0 +1,13 @@
+package picpay.com.br.desafioBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/picpay/com/br/desafioBackend/DesafioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package picpay.com.br.desafioBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
